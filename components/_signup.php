49e4a3a45a79b340261f08c0c03528171c2e0418
naqash <kashisohtra@gmail.com>
2022-03-11 23:20:47 +0000

    Signup is working and created users table to store userInfo

--- components/_signup.php
+++ components/_signup.php
@@ -10,35 +10,87 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <form>
+                <form action="/PHP-FORUM/index.php" method="POST">
                     <div class="mb-3">
                         <label for="exampleInputEmail1" class="form-label">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp">
                         <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
                     </div>
                     <div class="mb-3">
                         <label for="username" class="form-label">Username</label>
-                        <input type="text" class="form-control" id="username">
+                        <input type="text" class="form-control" id="username" name="username">
                     </div>
                     <div class="mb-3">
                         <label for="exampleInputPassword1" class="form-label">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1">
+                        <input type="password" class="form-control" id="password" name="password">
                     </div>
                     <div class="mb-3">
                         <label for="CexampleInputPassword1" class="form-label">Confrim Password</label>
-                        <input type="password" class="form-control" id="cpassword">
-                    </div>
-                    <div class="mb-3 form-check">
-                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                    </div>
+                        <input type="password" class="form-control" id="cpassword" name="cpassword">
+                    </div>                   
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
-            </div>
+            </div>           
         </div>
     </div>
 </div>
+
+<?php 
+    $showalert = false;
+    $showerror = false;
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        include '_conn.php';
+        $email=$_POST['email'];
+        $username =$_POST['username'];
+        $password=$_POST['password'];
+        $cpassword=$_POST['cpassword'];
+
+        //check if the email alredy exist
+
+        $sql= "SELECT * FROM `users` WHERE email='$email'";
+
+        $result=mysqli_query($conn,$sql);
+        $numRow = mysqli_num_rows($result);
+
+        if($numRow>0){
+            $showerror ="Email already in use";
+        }
+        else{
+            if($password == $cpassword){
+                $hash = password_hash($password, PASSWORD_DEFAULT);
+                $store = "INSERT INTO `users` (`email`, `username`, `password`, `timestamp`) VALUES ('$email', '$username', '$hash', current_timestamp())";
+                $r= mysqli_query($conn,$store);
+                if($r){
+                    $showalert = true;
+                    //header("Location: /index.php?signupsuccess=true");
+                }
+            }
+            else{
+                $showerror= "Passwords do not match";
+                
+            }
+        }
+        //header("Location: /PHP-FORUM/index.php?signupsuccess=false&error=$showError");
+
+    }
+
+?>
+
+<!-- alert message to showcase successful signup -->
+<?php
+        if($showalert)
+            {    
+            echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+            <strong>SignUp Successful!</strong> Your account has been created successfully, You can now Login!
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>';
+            }
+            // alert message for not succesful signup
+            if($showerror)
+            {    
+            echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <strong>Error!</strong> '. $showerror.'
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>';
+            }
+    ?>
