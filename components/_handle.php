2ef57f14a413eb8c93cb23112d211a5afea6ab13
naqash <kashisohtra@gmail.com>
2022-03-12 16:39:15 +0000

    User can now login and displays what user is logged in

---
+++ components/_handle.php
@@ -0,0 +1,50 @@
+
+<?php
+    $login = false;
+    $showerror = false;
+    if($_SERVER['REQUEST_METHOD']=="POST"){
+        include '_conn.php';
+        $email=$_POST["email"];
+        $pass=$_POST["password"];
+
+        $sql="SELECT * FROM `users` WHERE email='$email'";
+
+        $result=mysqli_query($conn,$sql);
+
+        $num=mysqli_num_rows($result);
+
+        if($num==1){
+            $row = mysqli_fetch_assoc($result);
+            if(password_verify($pass, $row['password'])){
+                echo"loggedin";
+                $login=true;
+                session_start();
+                $_SESSION['loggedin']=true;
+                $_SESSION['email']=$email;
+                
+                
+                header("Location: /PHP-FORUM/index.php");
+            } else{
+                $showerror="invalid username or password";
+            }
+
+        }
+    }
+    
+?>
+
+<?php
+    // if ($login) {
+    // echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+    //     <strong>Login Successful!</strong> You are logged in successfully!
+    //     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    //     </div>';
+    // }
+    // // alert message for not succesful signup
+    // if ($showerror) {
+    // echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+    //     <strong>Error!</strong> ' . $showerror . '
+    //     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    //     </div>';
+    // }
+?>
--- components/_header.php
+++ components/_header.php
@@ -1,45 +1,58 @@
 <?php
+session_start();
+
 echo'  
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-<div class="container-fluid">
-<a class="navbar-brand" href="index.php">Lets Discuss</a>
-<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-<span class="navbar-toggler-icon"></span>
-</button>
-<div class="collapse navbar-collapse" id="navbarSupportedContent">
-<ul class="navbar-nav me-auto mb-2 mb-lg-0">
-  <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+  <div class="container-fluid">
+  <a class="navbar-brand" href="index.php">Lets Discuss</a>
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+  <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+    <li class="nav-item">
+      <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">About</a>
+    </li>
+    <li class="nav-item">
+    <a class="nav-link" href="contact.php">Contact</a>
   </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">About</a>
-  </li>
-  <li class="nav-item">
-  <a class="nav-link" href="contact.php">Contact</a>
-</li>
-  <li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-      Categories
-    </a>
-    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-      <li><a class="dropdown-item" href="#">Action</a></li>
-      <li><a class="dropdown-item" href="#">Another action</a></li>
-      <li><hr class="dropdown-divider"></li>
-      <li><a class="dropdown-item" href="#">Something else here</a></li>
-    </ul>
-  </li>
-</ul>
-<div class=" mx-2">
-<form class="d-flex">
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        Categories
+      </a>
+      <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+        <li><a class="dropdown-item" href="#">Action</a></li>
+        <li><a class="dropdown-item" href="#">Another action</a></li>
+        <li><hr class="dropdown-divider"></li>
+        <li><a class="dropdown-item" href="#">Something else here</a></li>
+      </ul>
+    </li>
+  </ul>
+  <div class="mx-2">';
+  if(isset($_SESSION['loggedin']) && $_SESSION['loggedin']==true){
+  echo '<form class="d-flex justify-content-center ">
   <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
   <button class="btn btn-success" type="submit">Search</button>
-  <button type="button" class="btn btn-outline-success mx-3" data-bs-toggle="modal" data-bs-target="#loginModal">Login</button>
-  <button type="button" class="btn btn-outline-success " data-bs-toggle="modal" data-bs-target="#signupModal">SignUp</button>
-</form>        
-</div>
-</div>
+  <p class="text-light my-0 mx-5  justify-content-center d-inline p-2" >Welcome&nbsp&nbsp'.$_SESSION['email'].'</p><button class="btn btn-danger" type="submit">Logout</button></form>
+  ';
+  }
+  else
+  {
+    '<form class="d-flex">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-success" type="submit">Search</button>
+      <button type="button" class="btn btn-outline-success mx-3" data-bs-toggle="modal" data-bs-target="#loginModal">Login</button>
+      <button type="button" class="btn btn-outline-success " data-bs-toggle="modal" data-bs-target="#signupModal">SignUp</button>
+    </form> ';
+  }       
+  echo '</div>
+  </div>
 </div>
 </nav>';
+
 include 'components/_login.php';
 include 'components/_signup.php';
 
